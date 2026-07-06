50cb94a919b1355f112bd4364a6e22b63c90bdfa
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-06 20:54:02 +0800

    [common] adding default flags for test devices

--- config/perf/sm6375.mk
+++ config/perf/sm6375.mk
@@ -1,3 +1,7 @@
+TARGET_DISABLES_LIBPERF := true
+TARGET_SHIPS_AXION_KERNEL_MODULES := true
+TARGET_SUPPORTS_KERNEL_MANAGER := true
+
 ifeq ($(TARGET_SUPPORTS_KERNEL_MANAGER),true)
 PRODUCT_COPY_FILES += \
     device/axion/common/config/perf/ax_kernel_manager_sm6375.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/ax_kernel_manager.xml
