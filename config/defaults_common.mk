23d19ad65709f9b01e7c5ed60a357bdf00d779c5
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-06 20:46:06 +0800

    [ax_common] default commons migration

---
+++ config/defaults_common.mk
@@ -0,0 +1,33 @@
+TARGET_PRODUCT_PROP += \
+    device/axion/common/config/defaults_common.prop
+
+$(call inherit-product-if-exists, axion_sdk/ax_tflite/common.mk)
+
+PRODUCT_PACKAGES += \
+    AxThemePicker \
+    AxQuickLook \
+    AxionWidgets \
+    AxionParts \
+    AxThemeStore \
+    AxPcMode \
+    AxSandbox \
+    EdgeLauncher \
+    GameSpace \
+    OmniJaws \
+    ColumbusService
+
+# eliminate interpreter overhead
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    AxThemePicker \
+    AxionParts \
+    EdgeLauncher \
+    GameSpace \
+    AppLocker
+
+TARGET_INCLUDE_AXFX ?= false
+ifeq ($(TARGET_INCLUDE_AXFX),true)
+$(call inherit-product-if-exists, packages/apps/AxionFx/config.mk)
+endif
+
+$(call inherit-product-if-exists, packages/apps/FaceUnlock/common.mk)
