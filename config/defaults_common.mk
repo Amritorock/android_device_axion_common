bc2d6c1cb0a74c4b4244994b3504e342d0db3c92
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-07 06:41:49 +0800

    [vulkan-first] adding vulkan renderer enforcement

--- config/defaults_common.mk
+++ config/defaults_common.mk
@@ -35,3 +35,19 @@ $(call inherit-product-if-exists, packages/apps/AxionFx/config.mk)
 endif
 
 $(call inherit-product-if-exists, packages/apps/FaceUnlock/common.mk)
+
+#vulkan-first
+TARGET_USES_VULKAN := true
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.version-1_4.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+	frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
+	frameworks/native/data/etc/android.software.vulkan.deqp.level-2025-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
+	frameworks/native/data/etc/android.software.opengles.deqp.level-2025-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
+	
+TARGET_NEEDS_VULKAN_MEDIA_FIX ?= false
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.vk_use_ogl_for_media=$(TARGET_NEEDS_VULKAN_MEDIA_FIX)
