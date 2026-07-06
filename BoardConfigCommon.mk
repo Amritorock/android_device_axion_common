07ae02b1286388dda59cb0b7d7a555288ababe30
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-06 20:46:06 +0800

    [kernel_modules] add ax atcm

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -3,7 +3,8 @@ AXION_COMMON_MODULE_ROOT := $(AXION_COMMON_PATH)/kernel/modules
 AXION_COMMON_KERNEL_MODULES := \
     ax_thread_snooper \
     ax_affinity_guard \
-    ax_boost
+    ax_boost \
+    ax_atcm
 
 ifneq ($(strip $(TARGET_KERNEL_SOURCE)),)
 ifeq ($(strip $(TARGET_PREBUILT_KERNEL)),)
