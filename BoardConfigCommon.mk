a3c3756cab526a5663bb81e1f7fd387b118d9593
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-06 20:46:06 +0800

    [kernel_modules] add ax boost

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -2,7 +2,8 @@ AXION_COMMON_PATH := device/axion/common
 AXION_COMMON_MODULE_ROOT := $(AXION_COMMON_PATH)/kernel/modules
 AXION_COMMON_KERNEL_MODULES := \
     ax_thread_snooper \
-    ax_affinity_guard
+    ax_affinity_guard \
+    ax_boost
 
 ifneq ($(strip $(TARGET_KERNEL_SOURCE)),)
 ifeq ($(strip $(TARGET_PREBUILT_KERNEL)),)
