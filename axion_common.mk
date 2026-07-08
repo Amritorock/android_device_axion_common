0ac4334f07087d28b47c1c9b9e54a9f5e358ea82
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-08 09:11:11 +0800

    [ax_common] migrate init cgroup policy

--- axion_common.mk
+++ axion_common.mk
@@ -4,4 +4,5 @@ TARGET_DISABLES_LIBPERF := true
 endif
 
 PRODUCT_COPY_FILES += \
+    device/axion/common/init/init.axion.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axion-common.rc \
     device/axion/common/init/init.axion.modules.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.axion.modules.rc
