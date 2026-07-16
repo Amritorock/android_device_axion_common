08cb7bf225821baddccc93827c45d24ab77cd781
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-16 09:05:58 +0800

    [vulkan] adding vulkan deps

--- config/defaults_common.mk
+++ config/defaults_common.mk
@@ -8,17 +8,7 @@ include device/axion/common/config/packages.mk
 include device/axion/common/config/flags.mk
 include device/axion/common/config/properties.mk
 include device/axion/common/config/product.mk
+include device/axion/common/config/vulkan/vulkan.mk
 
 $(call soong_config_set_bool,lmkd,use_hooks,true)
 
-#vulkan-first
-TARGET_USES_VULKAN := true
-
-PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
-	frameworks/native/data/etc/android.hardware.vulkan.version-1_4.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
-	frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
-	frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
-	frameworks/native/data/etc/android.software.vulkan.deqp.level-2025-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
-	frameworks/native/data/etc/android.software.opengles.deqp.level-2025-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml
-	
