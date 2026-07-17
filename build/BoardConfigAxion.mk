80bff9f5e76e1a7892f6b7143fe78c20b6b822db
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-18 06:35:14 +0800

    [kernel_modules] add missing flag for kernel modules inclusion

--- build/BoardConfigAxion.mk
+++ build/BoardConfigAxion.mk
@@ -1,3 +1,7 @@
 -include device/axion/common/config/soc_map.mk
+
+ifeq ($(TARGET_SHIPS_AXION_KERNEL_MODULES),true)
 include device/axion/common/config/board/kernel_modules.mk
+endif
+
 include device/axion/common/config/board/sepolicy.mk
