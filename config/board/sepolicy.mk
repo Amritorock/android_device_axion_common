115b297a0df174b55fb27c2bf92e5191c929b3bc
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-21 07:19:53 +0800

    [sepolicy] adding checks for qcom vendor sepolicies
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/board/sepolicy.mk
+++ config/board/sepolicy.mk
@@ -1,3 +1,5 @@
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/axion/common/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/axion/common/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += device/axion/common/sepolicy/vendor
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+BOARD_VENDOR_SEPOLICY_DIRS += device/axion/common/sepolicy/vendor/qcom
+endif
