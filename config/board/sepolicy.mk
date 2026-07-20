1a5d1e9bd8ae004b4de8b1f3927102b68597912d
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-21 06:59:41 +0800

    [sepolicy] adding vendor sepolicy for ax common

--- config/board/sepolicy.mk
+++ config/board/sepolicy.mk
@@ -1,2 +1,3 @@
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/axion/common/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/axion/common/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += device/axion/common/sepolicy/vendor
